09a8ea1b34166b19222ab9153aa8387d07fe96c0
Bear88TD <2115266@dlu.edu.vn>
2023-03-16 10:52:03 +0700

    Initial commit

---
+++ BaiTapThietKeForm/BaiTapThietKeForm/frmBai3.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace BaiTapThietKeForm
+{
+    partial class frmBai3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTuMoi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNghia = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtHienThi = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(140, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DANH SÁCH TỪ MỚI";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtTuMoi
+            // 
+            this.txtTuMoi.Location = new System.Drawing.Point(143, 12);
+            this.txtTuMoi.Multiline = true;
+            this.txtTuMoi.Name = "txtTuMoi";
+            this.txtTuMoi.Size = new System.Drawing.Size(197, 29);
+            this.txtTuMoi.TabIndex = 1;
+            this.txtTuMoi.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nghĩa của từ :";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtNghia
+            // 
+            this.txtNghia.Location = new System.Drawing.Point(143, 47);
+            this.txtNghia.Multiline = true;
+            this.txtNghia.Name = "txtNghia";
+            this.txtNghia.Size = new System.Drawing.Size(197, 29);
+            this.txtNghia.TabIndex = 1;
+            this.txtNghia.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(197, 82);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 2;
+            this.btnThem.Text = "Thêm từ mới";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(501, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "NGHĨA CỦA TỪ";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(71, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Từ mới:";
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtHienThi
+            // 
+            this.txtHienThi.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtHienThi.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtHienThi.Location = new System.Drawing.Point(430, 191);
+            this.txtHienThi.Multiline = true;
+            this.txtHienThi.Name = "txtHienThi";
+            this.txtHienThi.ReadOnly = true;
+            this.txtHienThi.Size = new System.Drawing.Size(257, 212);
+            this.txtHienThi.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(74, 191);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(293, 212);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // frmBai3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtHienThi);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtNghia);
+            this.Controls.Add(this.txtTuMoi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "frmBai3";
+            this.Text = "frmBai3";
+            this.Load += new System.EventHandler(this.frmBai3_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTuMoi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNghia;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtHienThi;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ BaiThucHanhBuoi1/BaiThucHanhBuoi1/MainForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace BaiThucHanhBuoi1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSaoChep = new System.Windows.Forms.TextBox();
+            this.btnSaoChep = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập tên của bạn :";
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(157, 15);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(179, 20);
+            this.txtTen.TabIndex = 1;
+            this.txtTen.TextChanged += new System.EventHandler(this.txtTen_TextChanged);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(157, 41);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "Xử lý";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Bạn đã nhập :";
+            // 
+            // txtSaoChep
+            // 
+            this.txtSaoChep.Location = new System.Drawing.Point(157, 103);
+            this.txtSaoChep.Name = "txtSaoChep";
+            this.txtSaoChep.ReadOnly = true;
+            this.txtSaoChep.Size = new System.Drawing.Size(179, 20);
+            this.txtSaoChep.TabIndex = 1;
+            // 
+            // btnSaoChep
+            // 
+            this.btnSaoChep.Location = new System.Drawing.Point(261, 41);
+            this.btnSaoChep.Name = "btnSaoChep";
+            this.btnSaoChep.Size = new System.Drawing.Size(75, 23);
+            this.btnSaoChep.TabIndex = 2;
+            this.btnSaoChep.Text = "Sao chép";
+            this.btnSaoChep.UseVisualStyleBackColor = true;
+            this.btnSaoChep.Click += new System.EventHandler(this.SaoChep_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSaoChep);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtSaoChep);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Chương trình đầu tiên";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSaoChep;
+        private System.Windows.Forms.Button btnSaoChep;
+    }
+}
+
